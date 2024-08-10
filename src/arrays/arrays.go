8b713e5fc6832b2abe65936eac22bd941786a595
dyxgou <diego5614yt@gmail.com>
2024-08-10 16:22:38 -0500

    Arrays

---
+++ src/arrays/arrays.go
@@ -0,0 +1,22 @@
+package arrays
+
+import "fmt"
+
+func Arrays() {
+	var ints [3]int
+
+	ints[0] = 1
+
+	fmt.Printf("ints : %v\n", ints)
+
+  // If you have an spare array, you can declare the positions you wanna set
+
+  var ints2 [12]int{
+  0:1,
+  5:15,
+  10:100
+
+}
+
+
+}
